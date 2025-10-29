4ba9c0583c942f663c8f3e23c05aa4f05b51580e
davidbohorquezr2 <davidaempress@outlook.com>
2025-10-29 15:52:24 -0500

    1
    1

---
+++ Taller5/demo/src/main/java/com/example/Usuario.java
@@ -0,0 +1,23 @@
+package com.example;
+
+public class Usuario {
+    protected String usuario;
+    protected String contrasena;
+    protected String nombre;
+    protected String apellido;
+
+    public Usuario(String usuario, String contrasena, String nombre, String apellido) {
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.contrasena = contrasena;
+    }
+
+    protected boolean logIn() {
+        
+    }
+
+    protected boolean logOut() {
+        
+    }
+}
